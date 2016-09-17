757946138ace5e12e0b1dc6fef380f402110ed9e
Daniel Luxemburg <daniel.luxemburg@gmail.com>
2016-09-17 17:19:08 -0400

    example error

--- demo/code/node-example.js
+++ demo/code/node-example.js
@@ -1,4 +1,4 @@
 console.log('console.log');
 console.error('console.error');
 setTimeout(() => process.stdout.write('stdout (250ms)'), 250);
-setTimeout(() => process.stderr.write('stdout (750ms)'), 750);
+setTimeout(() => process.stderr.write('stderr (750ms)'), 750);
